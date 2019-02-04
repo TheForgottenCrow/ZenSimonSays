cb67e8f331cc291df5e5bfa522eee074db8860d4
Unknown <jeftasbrotherhood@gmail.com>
2019-02-04 14:22:28 +0100

    Added Placeholder scropts

---
+++ Assets/Code/LilliePad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LilliePad : MonoBehaviour
+{
+    [SerializeField] int m_Id;
+    [SerializeField] GameMaster m_Master;
+
+    private void OnMouseDown()
+    {
+        m_Master.Log(m_Id.ToString() + "Pressed");
+    }
+}
